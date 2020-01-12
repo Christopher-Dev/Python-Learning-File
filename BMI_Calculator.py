0f04c7fb7f0e72fe7d497ee3eecad9e3672fb9ec
cpinkus95 <52015592+cpinkus95@users.noreply.github.com>
2020-01-12 00:49:02 -0500

    BMI calculator

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,2 +1,16 @@
-print ('Hello World')
+print ('bmi Calculator')
+print('----------------------------------')
 
+name = "Chris"
+height_m = 2
+weight_kg = 264
+
+bmi = weight_kg / (height_m * height_m)
+print('bmi: ')
+print(bmi)
+if bmi < 25:
+    print('name')
+    print ('is not over weight')
+else:
+    print('chris')
+    print('is over weight')
